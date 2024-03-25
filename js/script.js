58b2f879564b0e1f451ade002b68d10c5482c2ab
suicidi0 <124095966+suicidi0@users.noreply.github.com>
2024-03-25 20:27:03 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function login() {
+  // Add your login logic here
+  console.log("Login button clicked");
+}
+
+function register() {
+  // Add your register logic here
+  console.log("Register button clicked");
+}
