1473b2f7a52b4c1333ea3d8c03793aa8366e92bf
suicidi0 <suicidiocontato@gmail.com>
2024-04-10 20:55:18 -0300

    ultima atualização
    local storage e css reorganizado

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # website :]
+
+
+##TODO:
+-> JSON Database;
+-> Visual rework (colors, background e etc);
+-> Two factor Authentication
+-> Add more features?
--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ function login() {
     return
   }
 
-  //Debug message
-  alert("Succcessfully logged in.")
+  // Debug message
+  alert("Successfully logged in.");
 
   console.log("Login button clicked");
 }
@@ -35,6 +35,11 @@ function signup() {
     return;
   }
 
+  if (password.includes(' ')) {
+    alert("Password should not contain spaces")
+    return;
+  }
+
   // Check if email is null or does not match the pattern
   var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(email)) {
@@ -48,9 +53,41 @@ function signup() {
     return;
   }
 
-  //DATA WILL BE INCLUDED HERE
-  //SQL? MONGO? LOCAL DATA?
+  // Essa parte de storage vai ficar infuncional com commits, caso queiram fazer testes simples é só descomittar :]
+  // Essa parte vai ser reescrevida com o banco de dados (MongoDB.org)
 
+  // Storage start
+  // Create a new user object
+  // var newUser = {
+  //   firstName: firstName,
+  //   lastName: lastName,
+  //   username: username,
+  //   email: email,
+  //   password: password
+  // };
+
+  // Load existing profiles or initialize an empty array
+  // var existingProfiles = localStorage.getItem("profiles");
+  // var profilesArray = existingProfiles ? JSON.parse(existingProfiles) : [];
+
+  // Check if the username or email already exists
+  // var userExists = profilesArray.some(function(profile) {
+  //   return profile.username === username || profile.email === email;
+  // });
+
+  // if (userExists) {
+  //   alert("Username or email already exists");
+  //   return;
+  // }
+
+  // Add the new user to the profiles array
+  // profilesArray.push(newUser);
+
+  // Update the profiles in local storage
+  // localStorage.setItem("profiles", JSON.stringify(profilesArray));
+  // Storage end
+
+  // Redirect to the index page
   window.location.href = "index.html";
 }
 
